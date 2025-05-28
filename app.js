61f1af5e3e822a7d1613c809f28eaaee0aa55b62
Malcolm <mstone@rollins.edu>
2025-05-28 17:01:26 -0400

    use morgan for http logs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const morgan = require("morgan")
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ const upload = multer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan("dev"))
 
 app.post("/upload", upload.any(), (req, res) => {
   console.log("Files:", req.files);
