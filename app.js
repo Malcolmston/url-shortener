3a4669d5ee9f0a6485fe243f4dc7c0149ff934c6
Malcolm <mstone@rollins.edu>
2025-05-28 16:56:42 -0400

    basic upload this is the basic upload structure using node multer

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
-require("dotenv").config()
+const multer = require("multer");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const upload = multer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT)
+app.post("/upload", upload.any(), (req, res) => {
+  console.log("Files:", req.files);
+  console.log("Body:", req.body);
+  res.json({ files: req.files, body: req.body });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
