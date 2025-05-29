15c2324ccf0a691648aa39bc384e5f5ae452212e
Malcolm <mstone@rollins.edu>
2025-05-28 20:52:21 -0400

    link the 2

---
+++ database/associations.ts
@@ -0,0 +1,15 @@
+import User from './User';
+import File from './File';
+
+// If each user has many files
+User.hasMany(File, {
+    foreignKey: 'userId',
+    as: 'files'
+});
+
+File.belongsTo(User, {
+    foreignKey: 'userId',
+    as: 'user'
+});
+
+export {User, File};
